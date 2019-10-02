07ff480e6caf42189373a40919bd58f03fc20836
LineBot-2019 <yalihiceg@mailspro.net>
2019-10-02 15:39:09 +0200

    modification from pi

--- Recherche/test_odometrie.py
+++ Recherche/test_odometrie.py
@@ -14,7 +14,7 @@ dxl_io.set_joint_mode([const.left_motor_id, const.right_motor_id])
 odom = co.odometrie()
 t=0
 while(true):
-  if(t == fmod(const.delta_t,10))
+  if(t == fmod(const.delta_t,10)):
     dxl_io.get_moving_speed({1})
     dxl_io.get_moving_speed({2})
   t += const.delta_x
